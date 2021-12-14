79008a04e47b36ca4634512840dc130398f2fe15
Szymon Flis <szmflis@gmail.com>
2021-12-14 03:47:01 +0100

    up

--- zajecia8-9/skrypt.ps1
+++ zajecia8-9/skrypt.ps1
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77f96c57e08907233000cde8fd2e6b62713607cee1f2362e2fc96beeb1053b90
-size 4850
+oid sha256:01f23619cc90096aa41586bc18af64af07304940b3a33affdd5fae8af04e7435
+size 5900
