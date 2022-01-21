0ad07a84c66657241f1e6bf038550f99dc5b0c9e
Szymon Flis <szmflis@gmail.com>
2022-01-21 08:15:41 +0100

    zajecia 11

---
+++ zajecia11/mailSender.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1cfd9fdb5ddbdd8058f471699f75e8d8ae0e2ba30541ab3b3f59f2ddbf083baa
+size 1207
---
+++ zajecia11/mail_problem.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dea02815bbf7c9f015d46e148793c092410162577deb9935b1f7c68ddc719bca
+size 2448
